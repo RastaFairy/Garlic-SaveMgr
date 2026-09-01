4ca9b0b1330fb65d445ebdc033d5887a3019a96e
RastaFairy <mikielprofe@gmail.com>
2026-09-02 00:49:14 +0200

    Delete GarlicSaveMgr directory

--- GarlicSaveMgr/Infrastructure/AppInfo.cs
+++
@@ -1,7 +0,0 @@
-namespace GarlicSaveMgr.Infrastructure;
-
-public static class AppInfo
-{
-    public const string Version = "6.8";
-    public const string UserAgent = "GarlicSaveMgr/6.8";
-}
--- GarlicSaveMgr/Services/GarlicException.cs
+++
@@ -1,7 +0,0 @@
-namespace GarlicSaveMgr.Services;
-
-public sealed class GarlicException : Exception
-{
-    public GarlicException(string message) : base(message) { }
-    public GarlicException(string message, Exception inner) : base(message, inner) { }
-}
